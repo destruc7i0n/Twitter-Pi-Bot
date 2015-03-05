4600a621b29360d87d8d763af3f7132ee6693201
destruc7i0n <dscdsouza@outlook.com>
2015-03-05 10:29:03 -0500

    Update pi.py

--- pi.py
+++ pi.py
@@ -2,7 +2,7 @@
 Placeholder for now
 '''
 import tweepy
-from tweepy import Stream
+from tweepy import Stream #To get a live Stream of tweets
 from decimal import *
 
 TWITTER_CONSUMER_KEY = "TWITTER_CONSUMER_KEY"
@@ -10,10 +10,24 @@ TWITTER_CONSUMER_SECRET = "TWITTER_CONSUMER_SECRET"
 TWITTER_ACCESS_KEY = "TWITTER_ACCESS_KEY"
 TWITTER_ACCESS_SECRET = "TWITTER_ACCESS_SECRET"
 
+TWITTER_HANDLE = "@tweetedpi" #You may change based on the handle
+
+#Now to authenticate
+auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
+auth.set_access_token(TWITTER_ACCESS_KEY, TWITTER_ACCESS_SECRET)
+api = tweepy.API(auth)
+
 pi = Decimal("3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196")
 #First 200 digits of Pi after the decimal
 
+class StreamListener():
+  '''
+  The main code
+  '''
+
 def main():
-  
+  '''
+  Just to activate Listener
+  '''
 
 if __name__ == "__main__": main()
