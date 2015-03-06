9172c20990be6537e784f124d94f867830db81d0
destruc7i0n <dscdsouza@outlook.com>
2015-03-06 09:52:23 -0500

    Finished

--- pi.py
+++ pi.py
@@ -5,9 +5,9 @@ A basic Twitter Bot to tweet Pi to anyone who tweets to it.
 try: import simplejson as json #Get a faster version of json, if this fails just import normal json
 except ImportError: import json
 
-import tweepy, time
-from tweepy import Stream, StreamListener #To get a live Stream of tweets
-from decimal import * #To puty pi into a decimal
+import tweepy, time #For timing
+from tweepy import Stream, StreamListener #To get a live stream of tweets that you can reply to
+from decimal import Decimal #To put pi into a decimal
 
 METHOD = "CONSOLE"
 TWITTER_CONSUMER_KEY = "TWITTER_CONSUMER_KEY"
@@ -16,7 +16,7 @@ TWITTER_ACCESS_KEY = "TWITTER_ACCESS_KEY"
 TWITTER_ACCESS_SECRET = "TWITTER_ACCESS_SECRET"
 SELF_ID = "1234567890" #Find this when making the app on Twitter
 
-TWITTER_HANDLE = "@tweetedpi" #You may change based on the handle
+TWITTER_HANDLE = "@tweetedpi" #You may change based on your handle
 
 #Now to authenticate
 auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
@@ -31,7 +31,7 @@ class customStreamListener(StreamListener):
 	The main code
 	'''
 	def on_data(self, data):
-		print "%s: Started at %s" % (METHOD, time.ctime())
+		print "%s: Started at %s" % (METHOD, time.ctime()) #Debug
 		jdata = json.loads(data.strip())
 		print METHOD+": Tweet from @"+jdata.get("user",{}).get("screen_name")+": "+jdata.get("text")
 		retweeted = jdata.get("retweeted")
@@ -41,18 +41,18 @@ class customStreamListener(StreamListener):
 			'''
 			Now for the real part, the part before is just to make sure that the bot should answer
 			'''
-			reply_to = jdata.get('user',{}).get('screen_name')
-			reply_to_id = jdata.get("user",{}).get("id_str","")
+			reply_to = jdata.get('user',{}).get('screen_name') #Get screen name for whom to reply to
+			reply_to_id = jdata.get("user",{}).get("id_str","") #Get id of user to reply to
 			begin = 0
-			end = 138-len(jdata.get('user',{}).get('screen_name'))
-			tweet = "@"+reply_to+" "+str(pi)[begin:end]
-			api.update_status(tweet, in_reply_to_status_id = reply_to_id)
+			end = 138-len(jdata.get('user',{}).get('screen_name')) #Get as much pi as it can into a tweet
+			tweet = "@"+reply_to+" "+str(pi)[begin:end] #Get pi and put it into a tweet
+			api.update_status(tweet, in_reply_to_status_id = reply_to_id) #This replies
 			print METHOD+": Replied to @"+reply_to+" with '"+str(pi)[begin:end]+"' ("+str(len(str(pi)[begin:end]))+" digits)" #Print how many digits of pi printed and the tweet
-			print "%s: Ended at %s" % (METHOD, time.ctime())
+			print "%s: Ended at %s" % (METHOD, time.ctime()) #Debug
 
 	def on_error(self, error):
-		print METHOD+": "+str(error)
-		time.sleep(5)
+		print METHOD+": "+str(error) #Print error
+		time.sleep(5) #Wait 5 seconds and try again
 		return True
 		
 
